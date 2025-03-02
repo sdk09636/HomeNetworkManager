4a89cd6f9db84b6a1f533b9f20b3f2c642c98429
Shubham Kodilkar <shubhamkodilkar@gmail.com>
2025-03-02 08:50:48 -0500

    Added iptables config file

---
+++ firewall.sh
@@ -0,0 +1,7 @@
+iptables -t nat -A PREROUTING -p udp -dport 53 -j REDIRECT --to-ports 53
+iptables -t nat -A PREROUTING -p tcp -dport 53 -j REDIRECT --to-ports 53
+
+iptables-save > /etc/sysconfig/iptables
+
+sudo systemctl restart iptables
+sudo systemctl enable iptables
