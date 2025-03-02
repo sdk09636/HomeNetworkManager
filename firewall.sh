5778c7da83895fbeeba52d1be69d2fc592f94c7b
Shubham Kodilkar <shubhamkodilkar@gmail.com>
2025-03-02 09:35:36 -0500

    Modified firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,5 +1,5 @@
-iptables -t nat -A PREROUTING -p udp -dport 53 -j REDIRECT --to-ports 53
-iptables -t nat -A PREROUTING -p tcp -dport 53 -j REDIRECT --to-ports 53
+iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53
+iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 53
 
 iptables-save > /etc/sysconfig/iptables
 
