2d588a4239d02e608fd40c278020e2ca52abf08a
sdk09636 <shubhamkodilkar@gmail.com>
2025-05-20 23:35:21 +0530

    Error correction

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
-const { exec } = require('child_process');
+const { exec, spawn } = require('child_process');
+const readline = require("readline");
 
 const app = express();
 const PORT = 3000;
